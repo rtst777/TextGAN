45f7b7a9b42ebbabc8376fa60a121158cf230037
Yizhan Jiang <yizhan.jiang@gmail.com>
2019-10-20 00:18:30 -0400

    added synthetic data generator

---
+++ dataset/synthetic_data_generator.py
@@ -0,0 +1,62 @@
+import random
+
+
+OPERATIONS = ['+', '-', '*', '/', '%', '^', '|', '&']
+OPERANDS = ['x', 'y', 'z', 'm', 'n', 'k', 'p', 'q']
+
+
+def generate_synthetic_data(num_data=100, max_sequence_len=1, num_operations=4, num_operands=1, early_stop=False):
+    """Generates synthetic data sequence using simple context-grammar.
+
+    Each sequence obeys the grammar rule: S -> operand || S operator S
+
+    Args:
+        num_data: The number of data sequence to generate.
+        max_sequence_len: The maximum length of each sequence.
+        num_operations: The number of operations that will be used in data generation.
+        num_operands: The number of operands that will be used in data generation.
+        early_stop: If true, the sequence length might be less than max_sequence_len; And the short sequence will be
+            padded with space.
+    """
+    assert num_data > 0, 'num_data should be positive'
+    assert max_sequence_len >= 1, 'max_sequence_len should be >= 1'
+    assert num_operations > 0, 'num_operations should be positive'
+    assert num_operations < len(OPERATIONS), 'num_operations should be <= %d' % len(OPERATIONS)
+    assert num_operands > 0, 'num_operands should be positive'
+    assert num_operands < len(OPERANDS), 'num_operands should be <= %d' % len(OPERANDS)
+
+    if (max_sequence_len - 1) % 2 != 0:
+        early_stop = True
+        print('Turn on early_stop for generating grammarly correct sequence')
+
+    operations = OPERATIONS[:num_operations] + [' '] if early_stop else OPERATIONS[:num_operations]
+    operands = OPERANDS[:num_operands]
+
+    file_name = 'synthetic_dataset_%d_data_%d_maxlen_%d_operations_%d_operands' % (
+    num_data, max_sequence_len, num_operations, num_operands)
+    if early_stop:
+        file_name += '_with_earlystop'
+
+    with open(file_name + '.txt', 'w+') as f:
+        for line in range(num_data):
+            sequence = random.choice(operands)
+            for i in range(1, max_sequence_len, 2):
+                next_op = random.choice(operations)
+                if next_op == ' ' or i == max_sequence_len - 1:
+                    sequence.ljust(max_sequence_len)
+                    break
+
+                sequence += next_op
+                sequence += random.choice(operands)
+
+            f.write(sequence + '' if line == num_data - 1 else sequence + '\n')
+
+
+def main():
+    generate_synthetic_data(num_data=10000, max_sequence_len=3, num_operations=4, num_operands=1, early_stop=False)
+    generate_synthetic_data(num_data=10000, max_sequence_len=15, num_operations=4, num_operands=1, early_stop=False)
+    generate_synthetic_data(num_data=10000, max_sequence_len=15, num_operations=4, num_operands=1, early_stop=True)
+
+
+if __name__ == "__main__":
+    main()
