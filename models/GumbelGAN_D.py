7c792b7dbcd6e8e10eaa567078e2c19f60ab0a41
EEEEEEEhan7 <yizhan.jiang@gmail.com>
2019-10-21 00:03:29 -0400

    minor code factory for GumbelGAN_D

--- models/GumbelGAN_D.py
+++ models/GumbelGAN_D.py
@@ -4,11 +4,16 @@ import torch.nn.functional as F
 
 from models.discriminator import CNNDiscriminator
 
+
 dis_filter_sizes = [2, 3, 4, 5]
 dis_num_filters = [300, 300, 300, 300]
+dis_filter_sizes2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
+dis_num_filters2 = [100, 200, 200, 200, 200, 100, 100, 100, 100, 100, 160, 160]
 
 
 class GumbelGAN_D(CNNDiscriminator):
+    '''The discriminator from RelGAN.'''
+
     def __init__(self, embed_dim, max_seq_len, num_rep, vocab_size, padding_idx, gpu=False, dropout=0.25):
         super(GumbelGAN_D, self).__init__(embed_dim, vocab_size, dis_filter_sizes, dis_num_filters, padding_idx,
                                        gpu, dropout)
@@ -51,3 +56,14 @@ class GumbelGAN_D(CNNDiscriminator):
         logits = self.out2logits(pred).squeeze(1)  # [batch_size * num_rep]
 
         return logits
+
+
+class GumbelGAN_D2(CNNDiscriminator):
+    '''Ordinary CNN-based discriminator.'''
+
+    def __init__(self, embed_dim, max_seq_len, num_rep, vocab_size, padding_idx, gpu=False, dropout=0.25):
+        super(GumbelGAN_D2, self).__init__(embed_dim, vocab_size, dis_filter_sizes2, dis_num_filters2, padding_idx,
+                                       gpu, dropout)
+
+        self.feature2out = nn.Linear(self.feature_dim, 1)  # For 'rsgan' loss
+        # self.feature2out = nn.Linear(self.feature_dim, 2)  # For 'JS' loss
