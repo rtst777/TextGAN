fixed gradient backprop issues so that rebargan can run successfully

models/RebarGAN_G.py
@@ -9,15 +9,11 @@ class RebarGAN_G(LSTMGenerator):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, max_seq_len, padding_idx, temperature, gpu=False):
         super(RebarGAN_G, self).__init__(embedding_dim, hidden_dim, vocab_size, max_seq_len, padding_idx, gpu)
         self.name = 'rebargan'
-
-        self.temperature = temperature
-        # TODO(ethanjiang) switch to tunable temperature by uncommenting below code
-        # self.temperature = torch.tensor(temperature, requires_grad=True).float()
-        # if gpu:
-        #     self.temperature = self.temperature.cuda()
-
+        self.temperature = torch.tensor(temperature, dtype=torch.float, requires_grad=True)
+        if gpu:
+            self.temperature = self.temperature.cuda()
         # θ parameter in the REBAR equation. It is the softmax probability of the Generator output.
-        self.theta = torch.zeros(self.max_seq_len, self.vocab_size).long()
+        self.theta = None
 
 
     def batchPGLoss(self, inp, target, reward):
@@ -44,7 +40,7 @@ class RebarGAN_G(LSTMGenerator):
     def set_temperature_gradient(self, temperature_grad):
         '''Sets temperature gradient.'''
 
-        assert self.theta.shape == temperature_grad.shape, 'temperature_grad has different shape with self.temperature'
+        assert self.temperature.shape == temperature_grad.shape, 'temperature_grad has different shape with self.temperature'
         self.temperature.grad = temperature_grad.clone().detach()
 
 
@@ -56,7 +52,7 @@ class RebarGAN_G(LSTMGenerator):
         :return θ: max_seq_length * vocab_size
         """
 
-        theta_logit = torch.zeros(self.max_seq_len, self.vocab_size).long()
+        theta_logit = torch.zeros(self.max_seq_len, self.vocab_size, dtype=torch.float)
         hidden = self.init_hidden(1)
         inp = torch.LongTensor([start_letter])
         if self.gpu:
@@ -79,7 +75,8 @@ class RebarGAN_G(LSTMGenerator):
         :param estimated_gradient: estimated gradient for theta with respect to the loss. Shape: seq_len * vocab_size
         :return loss: REBAR loss
         """
-        assert self.theta.shape == estimated_gradient.shape, 'estimated_gradient has different shape with self.theta'
+        assert self.theta is not None and \
+               self.theta.shape == estimated_gradient.shape, 'estimated_gradient has different shape with self.theta'
 
         rebar_loss_matrix = self.theta * estimated_gradient
         rebar_loss = rebar_loss_matrix.sum()

utils/rebar_gradient_estimator.py
@@ -110,7 +110,7 @@ class RebarGradientEstimator:
         """
         mask = F.one_hot(b, cfg.vocab_size).bool()  # Shape: batch_size * seq_len * vocab_size
         log_pb = torch.log(z[mask]).sum()  # scalar
-        log_pb.backward()
+        log_pb.backward(retain_graph=True) # We don't want z get freed.
         gradient = theta.grad.clone().detach()
         theta.grad = torch.zeros(theta.shape)
         return gradient
@@ -151,7 +151,7 @@ class RebarGradientEstimator:
         sigma_lambda_z_tilde_batch = self._compute_gumbel_softmax(z_tilde_batch,
                                                                   temperature)  # Shape: batch_size * seq_len * vocab_size
 
-        f_H_z_batch = self._environment_function(F.one_hot(b_batch, cfg.vocab_size))  # Shape: batch_size
+        f_H_z_batch = self._environment_function(F.one_hot(b_batch, cfg.vocab_size).float())  # Shape: batch_size
         f_sigma_lambda_z_tilde_batch = self._environment_function(sigma_lambda_z_tilde_batch)  # Shape: batch_size
         gradient_wrt_log_pb_batch = self._compute_gradient_of_theta_wrt_log_pb(theta_batch, b_batch,
                                                                                z_batch)  # Shape: batch_size * seq_len * vocab_size
@@ -166,4 +166,4 @@ class RebarGradientEstimator:
         expected_theta_gradient = theta_gradient_batch.mean(dim=0)  # Shape: seq_len * vocab_size
         expected_temperature_gradient = temperature.grad / self.batch_size  # Shape: scalar
 
-        return expected_theta_gradient, expected_temperature_gradient
+        return expected_theta_gradient.clone().detach(), expected_temperature_gradient.clone().detach()